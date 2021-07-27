cc5a15ffa20572d7a8d24d424136bb6c2598c28f
eexileddYT <87630983+eexileddYT@users.noreply.github.com>
2021-07-27 21:34:16 +0800

    Add files via upload

---
+++ lesson7HW.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 26 19:51:25 2021
+
+@author: 蔡侑廷
+"""
+
+score = [90, 96, 65, 78, 98, 99]
+
+
+
+i = 0
+
+while i<6:
+    print(score[i])
+    i = 1 + i
+    
+
+
+x = 0
+
+for x in range(6):
+    print(score[x])
